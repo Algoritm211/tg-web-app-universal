9a94f04fd090c4b2ff450f62b876352fd77bfff0
Alexey <algoritm211@gmail.com>
2024-05-25 18:48:11 +0300

    feat: add changes for flower shop example

--- test-bot/bot-instance.ts
+++ test-bot/bot-instance.ts
@@ -7,9 +7,17 @@ dotenv.config({path: path.resolve(__dirname, '..', '.env.local')});
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!)
 
-bot.on(message('text'), async (ctx) => {
+bot.start(async (ctx) => {
   // Using context shortcut
-  await ctx.reply(`Hello ${ctx.message.from.first_name}`)
+  await ctx.reply(`Hello <b>${ctx.message.from.first_name}</b>. Welcome to Flower Shop! 🌸🌷
+
+💐We are your personal flower shop on Telegram. Easily browse and order beautiful bouquets for any occasion with fast delivery and exclusive deals. 
+
+<b>Let's make your moments blossom!</b> 🌼✨
+
+💐Open the app below and choose your first bouquet💐
+⬇️⬇️⬇️ 
+`, {parse_mode: 'HTML'})
 })
 
 void bot.launch().then(() => console.log('BOT SUCCESSFULLY STARTED'))
