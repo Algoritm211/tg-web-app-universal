b79f11743acc5a1d5f500e0411ad82a0f3ee7b44
Alexey <algoritm211@gmail.com>
2024-05-25 18:56:28 +0300

    chore: cleanup

--- src/app/api/bot/webhook/route.ts
+++ src/app/api/bot/webhook/route.ts
@@ -1,10 +1,11 @@
-import {Telegraf} from "telegraf";
+import { Telegraf } from 'telegraf';
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
 
 bot.start(async (ctx) => {
   // Using context shortcut
-  await ctx.reply(`Hello <b>${ctx.message.from.first_name}</b>. Welcome to Flower Shop! 🌸🌷
+  await ctx.reply(
+    `Hello <b>${ctx.message.from.first_name}</b>. Welcome to Flower Shop! 🌸🌷
 
 💐We are your personal flower shop on Telegram. Easily browse and order beautiful bouquets for any occasion with fast delivery and exclusive deals. 
 
@@ -12,20 +13,23 @@ bot.start(async (ctx) => {
 
 💐Open the app below and choose your first bouquet💐
 ⬇️⬇️⬇️ 
-`, {parse_mode: 'HTML'})
-})
+`,
+    { parse_mode: 'HTML' }
+  );
+});
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const { searchParams } = new URL(request.url)
-    console.log(searchParams, body)
+    const { searchParams } = new URL(request.url);
+    console.log(searchParams, body);
 
     if (searchParams.get('setWebhook') === 'true') {
       const webhookUrl = `${process.env.VERCEL_URL}/api/bot/webhook`;
 
       const isSet = await bot.telegram.setWebhook(webhookUrl);
       console.log(`Set webhook to ${webhookUrl}: ${isSet}`);
+
       return Response.json('OK');
     }
 
