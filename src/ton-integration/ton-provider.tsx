04a6cfe2690428069b011e1cb73955ce27e396e3
Alexey <algoritm211@gmail.com>
2024-06-19 23:45:38 +0300

    fix: provider

--- src/ton-integration/ton-provider.tsx
+++ src/ton-integration/ton-provider.tsx
@@ -4,7 +4,7 @@ import React, { PropsWithChildren } from 'react';
 export const TonProvider: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <TonConnectUIProvider
-      manifestUrl={`${process.env.NEXT_PUBLIC_TEST_NGROK_URL}/manifest.json`}
+      manifestUrl={`https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}/manifest.json`}
       uiPreferences={{ theme: THEME.DARK, borderRadius: 's' }}
       actionsConfiguration={{
         twaReturnUrl: `https://t.me/${process.env.NEXT_PUBLIC_BOT_NAME}/cart`,
