f735d7e4d84ea7120abfd929b3bb5a6bff9a9bff
Alexey <algoritm211@gmail.com>
2024-08-22 21:10:36 +0300

    chore: return card payments

--- src/app/api/create-invoice-link/route.ts
+++ src/app/api/create-invoice-link/route.ts
@@ -1,38 +1,34 @@
 import { Product, ProductCartItem } from '@/config/types/entities';
-import { TELEGRAM_STARS_IN_ONE_DOLLAR, truncateString } from '@/shared';
 import { Telegram, Types } from 'telegraf';
 
 const bot = new Telegram(process.env.TELEGRAM_BOT_TOKEN!);
 
-const BASE_PROD_URL = 'https://tg-web-app-flower-shop-demo.vercel.app';
-
 export async function POST(request: Request) {
   const products = (await request.json()) as Product[] | ProductCartItem[];
 
-  const TITLE = 'your product';
+  const TITLE = 'Your order in SHOP NAME';
   const DESCRIPTION = 'Thank you very much for choosing us';
-
-  const totalPrice =
-    products.reduce((acc, product) => {
-      return acc + product.price.amount * ((product as ProductCartItem)?.count || 1);
-    }, 0) * TELEGRAM_STARS_IN_ONE_DOLLAR;
+  /**
+   * We can get currency from the first product because all items will be in the same currency
+   */
+  const currency = products[0].price.currency;
 
   const invoice: Types.NewInvoiceLinkParameters = {
     title: TITLE,
     description: DESCRIPTION,
     payload: 'payment-from-tg-bot',
-    // Telegram stars
-    currency: 'XTR',
-    prices: [
-      {
-        amount: totalPrice,
-        label: `all products price`,
-      },
-    ],
-    photo_url: `${BASE_PROD_URL || process.env.VERCEL_URL || process.env.NEXT_PUBLIC_TEST_NGROK_URL || 'http://localhost:3000'}/invoice/invoice-stub-image.png`,
+    provider_token: process.env.PAYMENTS_PROVIDER_TOKEN!,
+    currency,
+    prices: products.map((product) => ({
+      amount: product.price.amount * ((product as ProductCartItem)?.count || 1),
+      label: `${product.name} ${(product as ProductCartItem).count || 1}x`,
+    })),
+    photo_url: `${process.env.VERCEL_URL || process.env.TEST_NGROK_URL || 'http://localhost:3000'}/invoice/invoice-stub-image.png`,
     photo_height: 800,
     photo_width: 800,
-  } as Types.NewInvoiceLinkParameters;
+  };
+
+  console.log('INVOICE', invoice);
 
   const link = await bot.createInvoiceLink(invoice);
 
