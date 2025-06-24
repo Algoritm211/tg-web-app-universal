d188267821c13a3d1709499d9fd346b5081c172d
Alexey <algoritm211@gmail.com>
2025-06-24 22:01:01 +0300

    chore: add prod url to create invoice link

--- src/app/api/create-invoice-link/route.ts
+++ src/app/api/create-invoice-link/route.ts
@@ -14,7 +14,7 @@ export async function POST(request: Request) {
    */
   const currency = products[0].price.currency;
 
-  console.log('URL', `https://${process.env.VERCEL_URL || process.env.NEXT_PUBLIC_TEST_NGROK_URL || 'http://localhost:3000'}/invoice/invoice-stub-image.png`)
+  console.log('URL', `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL || process.env.VERCEL_URL || process.env.NEXT_PUBLIC_TEST_NGROK_URL || 'http://localhost:3000'}/invoice/invoice-stub-image.png`)
 
   const invoice: Types.NewInvoiceLinkParameters = {
     title: TITLE,
